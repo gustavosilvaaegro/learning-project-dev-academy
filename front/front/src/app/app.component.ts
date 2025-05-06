812595872dd960bb56d0003a35a52e6eefa5099c
Andre Machado <andre.machado@aegro.com.br>
2025-05-06 11:09:28 -0300

    update css

--- front/front/src/app/app.component.ts
+++ front/front/src/app/app.component.ts
@@ -23,6 +23,7 @@ export class AppComponent {
   tableData: TableData[] = [];
   tableHeaders: string[] = [];
   imageFile: File | null = null;
+  loading: boolean = false;
 
   onDragOver(event: DragEvent) {
     event.preventDefault();
@@ -59,96 +60,126 @@ export class AppComponent {
 
   async uploadImage() {
     const fileInput = document.getElementById('imageInput');
-            const userEmailInput = document.getElementById('userEmail');
-            const resultsDiv = document.getElementById('results');
-            const files = (fileInput as HTMLInputElement).files;
-            const userEmail = (userEmailInput as HTMLInputElement).value;
-            if (!files || files.length === 0) {
-                //resultsDiv.innerHTML = 'Please select at least one file.';
-                return;
-            } 
+    const userEmailInput = document.getElementById('userEmail');
+    const resultsDiv = document.getElementById('results');
+    const files = (fileInput as HTMLInputElement).files;
+    const userEmail = (userEmailInput as HTMLInputElement).value;
+    if (!files || files.length === 0) {
+        return;
+    } 
+    if (!userEmail) {
+        alert('Por favor, insira seu e-mail.');
+        return;
+    }
 
-            
+    this.loading = true;
+    const formData = this.createFormData(files, userEmail);
 
-            const formData = new FormData();
-            for (let i = 0; i < files.length; i++) {
-                formData.append('files', files[i]); // 'files' deve corresponder ao @RequestParam no backend
-            }
-            formData.append('userEmail', userEmail); // 'userEmail' deve corresponder ao @RequestParam no backend
+    try {
+        const response = await fetch('http://localhost:8080/api/images/upload', {
+            method: 'POST',
+            body: formData,
+        });
+        if (response.ok) {
+          const data = await response.json();
+          this.createTable(data);
+          console.log(data);
+          this.missingFiles(data, files);
+          this.tableData
+        }
+    } catch (error) {
+      console.error('Erro ao fazer o upload da imagem:', error);
+    }
+    this.loading = false;
+  }
 
-            try {
-                const response = await fetch('http://localhost:8080/api/images/upload', {
-                    method: 'POST',
-                    body: formData,
-                });
-                if (response.ok) {
-                  const data = await response.json();
-                  console.log(data);
-                  
-                  this.tableHeaders = ['Nome do arquivo', 'data', 'Descontos Impureza %', 'Descontos Impureza kg', 'Peso Bruto kg', 'Peso Liquido kg', 'Nome Motorista', 'Placa Veículo', 'Desconto Umidade %', 'Desconto Umidade kg', 'Destino Armazem', 'Numero Romaneio', 'Produto', 'Produtor', 'Tara Kg', 'Total descontos kg'];
-                  if (Array.isArray(data)) {
-                    this.tableData = [];
-                      data.forEach((result, index) => {
-                          this.tableData[index] = {
-                              'Nome do arquivo': result.fileName,
-                              data: result.fileInfo.data,
-                              'Descontos Impureza %': result.fileInfo.descontosImpurezaPercentual,
-                              'Descontos Impureza kg': result.fileInfo.descontosImpurezaQuantidadeKg,
-                              'Peso Bruto kg': result.fileInfo.pesoBrutoKg,
-                              'Peso Liquido kg': result.fileInfo.pesoLiquidoKg,
-                              'Nome Motorista': result.fileInfo.nomeMotorista,
-                              'Placa Veículo': result.fileInfo.placaVeiculo,
-                              'Desconto Umidade %': result.fileInfo.descontosUmidadePercentual,
-                              'Desconto Umidade kg': result.fileInfo.descontosUmidadeQuantidadeKg,
-                              'Destino Armazem': result.fileInfo.destinoArmazem,
-                              'Numero Romaneio': result.fileInfo.numeroRomaneio,
-                              'Produto': result.fileInfo.produto,
-                              'Produtor': result.fileInfo.produtor,
-                              'Tara Kg': result.fileInfo.taraKg,
-                              'Total descontos kg': result.fileInfo.totalDescontosKg,
-                            }
-                              
-                          
-                    });
-                    if ((files.length - data.length) != 0) {
+  async getEmailData(){
+    const userEmailInput = document.getElementById('userEmail');
+    const userEmail = (userEmailInput as HTMLInputElement).value;
+    if (!userEmail) {
+        alert('Por favor, insira seu e-mail.');
+        return;
+    }
+    try {
+        const response = await fetch(`http://localhost:8080/api/images/files/email?userEmail=${userEmail}`, {
+            method: 'GET',
+        });
+        if (response.ok) {
+          const data = await response.json();
+          this.createTable(data);
+          console.log(data);
+          this.tableData
+        }
+        
+    } catch (error) {
+      console.error('Erro ao solicitar dados', error);
+        
+    }
+    this.loading = false;
+  }
+  private createFormData(files: FileList, userEmail: string): FormData {
+    const formData = new FormData();
+    Array.from(files).forEach(file => formData.append('files', file));
+    formData.append('userEmail', userEmail);
+    return formData;
+  }
 
-                      let isPresent;
-                      let filesNotPresent: String[] = [];
-                      let msg = `Erro na leitura de ${files.length - data.length} arquivos:`;
-                  
-                      for (let i =0; i<files.length; i++) {
-                  
-                          isPresent = false;
-                  
-                          data.forEach(data => {
-                              if (files[i].name === data.fileName) {
-                                  isPresent = true;
-                              }
-                          });
-                  
-                          if (isPresent ===false) {
-                              filesNotPresent.push(files[i].name);
-                          }
-                      }
-                  
-                      for (let i = 0; i < filesNotPresent.length; i++) {
-                          msg += `\n-${filesNotPresent[i]}`;
-                      }
-                  
-                      alert(msg);
-                  
-                  }
-                    }
-
-                }
+  private createTable(data:any[]){
+    this.tableHeaders = ['Nome do arquivo', 'data', 'Descontos Impureza %', 'Descontos Impureza kg', 'Peso Bruto kg', 'Peso Liquido kg', 'Nome Motorista', 'Placa Veículo', 'Desconto Umidade %', 'Desconto Umidade kg', 'Destino Armazem', 'Numero Romaneio', 'Produto', 'Produtor', 'Tara Kg', 'Total descontos kg'];
+    if (Array.isArray(data)) {
+      this.tableData = [];
+        data.forEach((result, index) => {
+            this.tableData[index] = {
+                'Nome do arquivo': result.fileName,
+                data: result.fileInfo.data,
+                'Descontos Impureza %': result.fileInfo.descontosImpurezaPercentual,
+                'Descontos Impureza kg': result.fileInfo.descontosImpurezaQuantidadeKg,
+                'Peso Bruto kg': result.fileInfo.pesoBrutoKg,
+                'Peso Liquido kg': result.fileInfo.pesoLiquidoKg,
+                'Nome Motorista': result.fileInfo.nomeMotorista,
+                'Placa Veículo': result.fileInfo.placaVeiculo,
+                'Desconto Umidade %': result.fileInfo.descontosUmidadePercentual,
+                'Desconto Umidade kg': result.fileInfo.descontosUmidadeQuantidadeKg,
+                'Destino Armazem': result.fileInfo.destinoArmazem,
+                'Numero Romaneio': result.fileInfo.numeroRomaneio,
+                'Produto': result.fileInfo.produto,
+                'Produtor': result.fileInfo.produtor,
+                'Tara Kg': result.fileInfo.taraKg,
+                'Total descontos kg': result.fileInfo.totalDescontosKg,
+              }
                 
-            } catch (error) {
-              console.error('Error uploading image:', error);
-               
-            }
-      }
-  
+            
+      });
+    }
+  }
 
+  private missingFiles(data: any[], files: FileList) {
+    let isPresent;
+    let filesNotPresent: String[] = [];
+    let msg = `Erro na leitura de ${files.length - data.length} arquivos:`;
+
+    for (let i =0; i<files.length; i++) {
+
+        isPresent = false;
+
+        data.forEach(data => {
+            if (files[i].name === data.fileName) {
+                isPresent = true;
+            }
+        });
+
+        if (isPresent ===false) {
+            filesNotPresent.push(files[i].name);
+        }
+    }
+    if (filesNotPresent.length === 0) {
+        return;
+    }
+    for (let i = 0; i < filesNotPresent.length; i++) {
+        msg += `\n-${filesNotPresent[i]}`;
+    }
+    alert(msg);
+  }
 
 
   exportToExcel() {
